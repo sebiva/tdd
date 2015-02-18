6c7dcd5a9514cfcbe7e9074791e4f27a8a98b7e4
Sebastian Ivarsson <ivarsson.sebastian@gmail.com>
2015-02-18 15:22:20 +0100

    Chapter 4: Update functional test to test input

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,6 +16,22 @@ class NewVisitorTest(unittest.TestCase):
 
         # Check header
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Test input
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        # Adding something to the todo list
+        inputbox.send_keys('Buy food')
+        # When enter is pressed, the page should be updated and there should be an item
+        # "1: Buy food" in a todo list table
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == "1: Buy food" for row in rows))
+
         self.fail('Finish test!')
 
 
