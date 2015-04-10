3ec88ce5a018becb2676944e435f36b90e80d60e
Sebastian Ivarsson <ivarsson.sebastian@gmail.com>
2015-04-10 10:04:38 +0200

    Add a setup.py file, so the project can be set up with:
    python setup.py develop

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='Superlists',
+      version='0.1',
+      description='Todo list',
+      author='',
+      author_email='',
+      url='',
+      packages=['superlists'],
+      requires=['django', 'selenium'],
+      )
