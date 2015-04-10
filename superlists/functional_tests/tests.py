db728131dd423bf7d5c4390135eb7787f0395099
Sebastian Ivarsson <ivarsson.sebastian@gmail.com>
2015-04-10 10:51:31 +0200

    Update the FT to include another user and restrictions on urls

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -13,36 +13,66 @@ class NewVisitorTest(LiveServerTestCase):
     self.browser.quit()
 
   def test_can_start_a_list_and_retrieve_it_later(self):
-    # Open the page
-   self.browser.get(self.live_server_url)
+     # Open the page, first user, edith
+    self.browser.get(self.live_server_url)
 
-   # Check header
-   self.assertIn('To-Do', self.browser.title)
-   header_text = self.browser.find_element_by_tag_name('h1').text
-   self.assertIn('To-Do', header_text)
+    # Check header
+    self.assertIn('To-Do', self.browser.title)
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('To-Do', header_text)
 
-   # Test input
-   inputbox = self.browser.find_element_by_id('id_new_item')
-   self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-   # Adding something to the todo list
-   inputbox.send_keys('Buy feathers')
-   # When enter is pressed, the page should be updated and there should be an item
-   # "1: Buy feathers" in a todo list table
-   inputbox.send_keys(Keys.ENTER)
-   edith_list_url = self.browser.current_url
-   self.assertRegex(edith_list_url, '/lists/.+')
-   self.check_for_row_in_list_table('1: Buy feathers')
+    # Test input
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+    # Adding something to the todo list
+    inputbox.send_keys('Buy feathers')
+    # When enter is pressed, the page should be updated and there should be an item
+    # "1: Buy feathers" in a todo list table
+    inputbox.send_keys(Keys.ENTER)
+    edith_list_url = self.browser.current_url
+    self.assertRegex(edith_list_url, '/lists/.+')
+    self.check_for_row_in_list_table('1: Buy feathers')
 
-   # Another item is added to the list
-   inputbox = self.browser.find_element_by_id('id_new_item')
-   inputbox.send_keys('Use feathers and build')
-   inputbox.send_keys(Keys.ENTER)
+    # Another item is added to the list
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Use feathers and build')
+    inputbox.send_keys(Keys.ENTER)
 
-   # Now both items should be shown
-   self.check_for_row_in_list_table('1: Buy feathers')
-   self.check_for_row_in_list_table('2: Use feathers and build')
+    # Now both items should be shown
+    self.check_for_row_in_list_table('1: Buy feathers')
+    self.check_for_row_in_list_table('2: Use feathers and build')
 
-   self.fail('Finish test!')
+    ###########################################
+    # New user, Francis
+    ###########################################
+
+    # Use a new browser to make sure there is no info left about the old user.
+
+    self.browser.quit()
+    self.browser = webdriver.Firefox()
+
+    self.browser.get(self.live_server_url)
+    page_text = self.browser.find_element_by_tag_name('body').text
+    self.assertNotIn('Buy feathers', page_text)
+    self.assertNotIn('and build', page_text)
+
+    # The new user creates a new list by entering an item
+    inputbox = self.browser.find_element_by_tag_id('id_new_item')
+    inputbox.sendkeys('Buy milk')
+    inputbox.sendkeys(Keys.Enter)
+
+    # Francis gets his own url
+    francis_list_url = self.browser.current_url
+
+    self.fail('Finish test!')
+    self.assertRegex(francis_list_url, '/lists/.+')
+    self.assertNotEqual(francis_list_url, edith_list_url)
+
+    # Still no trace of old items
+    page_text = self.browser.find_element_by_tag_name('body').text
+    self.assertNotIn('Buy feathers', page_text)
+    self.assertNotIn('and build', page_text)
+
 
   def check_for_row_in_list_table(self, row_text):
     table = self.browser.find_element_by_id('id_list_table')
