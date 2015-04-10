87fcdb033790131db7876d2d7720def5e4f5dc06
Sebastian Ivarsson <ivarsson.sebastian@gmail.com>
2015-04-10 11:38:47 +0200

    Add url for creating new list, simplify home page

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -20,27 +20,6 @@ class HomePageTest(TestCase):
     expected_html = render_to_string('home.html')
     self.assertEqual(response.content.decode(), expected_html)
 
-  def test_home_page_can_save_a_POST_request(self):
-    request = HttpRequest()
-    request.method = 'POST'
-    request.POST['item_text'] = 'A new list item'
-
-    response = home_page(request)
-
-    self.assertEqual(Item.objects.count(), 1) # The item should now be added to the db.
-    new_item = Item.objects.first()
-    self.assertEqual(new_item.text, 'A new list item')
-
-  def test_home_page_redirects_after_POST(self):
-    request = HttpRequest()
-    request.method = 'POST'
-    request.POST['item_text'] = 'A new list item'
-
-    response = home_page(request)
-
-    self.assertEqual(response.status_code, 302)
-    self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
-
 class ItemModelTest(TestCase):
 
   def test_saving_and_retrieving_items(self):
@@ -60,22 +39,17 @@ class ItemModelTest(TestCase):
     self.assertEqual(first_saved_item.text, 'The first (ever) list item')
     self.assertEqual(second_saved_item.text, 'Item the second')
 
-  def test_home_page_only_saves_item_when_necessary(self):
-    request = HttpRequest()
-    home_page(request)
-    self.assertEqual(Item.objects.count(), 0)
-
 class ListViewTest(TestCase):
 
   def test_uses_list_template(self):
-    response = self.client.get('/lists/the-only-list-in-the-world')
+    response = self.client.get('/lists/the-only-list-in-the-world/')
     self.assertTemplateUsed(response, 'list.html')
 
   def test_displays_all_lists(self):
     Item.objects.create(text='itemey 1')
     Item.objects.create(text='itemey 2')
 
-    response = self.client.get('/lists/the-only-list-in-the-world')
+    response = self.client.get('/lists/the-only-list-in-the-world/')
 
     self.assertContains(response, 'itemey 1')
     self.assertContains(response, 'itemey 2')
@@ -83,7 +57,22 @@ class ListViewTest(TestCase):
     self.assertIn('itemey 1', response.content.decode())
     self.assertIn('itemey 2', response.content.decode())
 
+class NewListTest(TestCase):
 
+  def test_saving_a_POST_request(self):
+    self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+            )
 
+    self.assertEqual(Item.objects.count(), 1) # The item should now be added to the db.
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, 'A new list item')
 
+  def test_redirects_after_POST(self):
+    response = self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+            )
+    self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
